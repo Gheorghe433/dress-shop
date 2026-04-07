71306eef7504148d62027fe2ce8a3d8db63e2865
Gheorghe433 <gheorgherosca1337@gmail.com>
2026-04-07 12:17:18 +0300

    am adaugat functia de calculare a produselor

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ const content= {
       "Contact.html",
       "login.html",
       "wishlist.html",
+      
     ],
     textHTML:[
       "Home",
@@ -13,6 +14,7 @@ const content= {
       "Contact",
       "Login",
       "Wishlist",
+      
     ]
 }
 
